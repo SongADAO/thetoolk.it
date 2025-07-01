f6d46b529f7c86379394fd83c52ab87184660224
aLANparty <aLANparty@protonmail.com>
2025-07-01 10:54:02 -0400

    editor configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true,
+    staleTimes: {
+      dynamic: 0,
+    },
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
