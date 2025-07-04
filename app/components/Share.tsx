244bdba9c7cfabe83bac950a1147ebb53e85ee4c
aLANparty <aLANparty@protonmail.com>
2025-07-03 20:45:15 -0400

    share service settings

--- app/components/Share.tsx
+++ app/components/Share.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import { Suspense } from "react";
-
-import { ServiceAuthorizeWithContext } from "@/app/components/service/ServiceAuthorizeWithContext";
-import { ServiceFormWithContext } from "@/app/components/service/ServiceFormWithContext";
-import { ServiceRedirectHandlerWithContext } from "@/app/components/service/ServiceRedirectHandlerWithContext";
-import { ServiceSwitchWithContext } from "@/app/components/service/ServiceSwitchWithContext";
+import { ShareServiceSettings } from "@/app/components/ShareServiceSettings";
 import { InstagramContext } from "@/app/services/instagram/Context";
 import { InstagramProvider } from "@/app/services/instagram/Provider";
 import { S3Provider } from "@/app/services/s3/S3Provider";
@@ -20,27 +15,11 @@ export default function Share() {
           <InstagramProvider>
             <div className="flex flex-col gap-8">
               <div className="flex flex-col gap-1">
-                <ServiceSwitchWithContext
-                  context={YoutubeContext}
-                  form={<ServiceFormWithContext context={YoutubeContext} />}
-                />
-                <ServiceAuthorizeWithContext context={YoutubeContext} />
-                <Suspense>
-                  <ServiceRedirectHandlerWithContext context={YoutubeContext} />
-                </Suspense>
+                <ShareServiceSettings context={YoutubeContext} />
               </div>
 
               <div className="flex flex-col gap-1">
-                <ServiceSwitchWithContext
-                  context={InstagramContext}
-                  form={<ServiceFormWithContext context={InstagramContext} />}
-                />
-                <ServiceAuthorizeWithContext context={InstagramContext} />
-                <Suspense>
-                  <ServiceRedirectHandlerWithContext
-                    context={InstagramContext}
-                  />
-                </Suspense>
+                <ShareServiceSettings context={InstagramContext} />
               </div>
             </div>
           </InstagramProvider>
---
+++ app/components/ShareServiceSettings.tsx
@@ -0,0 +1,26 @@
+import { Context, Suspense } from "react";
+
+import { ServiceAuthorizeWithContext } from "@/app/components/service/ServiceAuthorizeWithContext";
+import { ServiceFormWithContext } from "@/app/components/service/ServiceFormWithContext";
+import { ServiceRedirectHandlerWithContext } from "@/app/components/service/ServiceRedirectHandlerWithContext";
+import { ServiceSwitchWithContext } from "@/app/components/service/ServiceSwitchWithContext";
+import type { ServiceContextType } from "@/app/services/ServiceContext";
+
+interface Props {
+  readonly context: Context<ServiceContextType>;
+}
+
+export function ShareServiceSettings({ context }: Props) {
+  return (
+    <>
+      <ServiceSwitchWithContext
+        context={context}
+        form={<ServiceFormWithContext context={context} />}
+      />
+      <ServiceAuthorizeWithContext context={context} />
+      <Suspense>
+        <ServiceRedirectHandlerWithContext context={context} />
+      </Suspense>
+    </>
+  );
+}
