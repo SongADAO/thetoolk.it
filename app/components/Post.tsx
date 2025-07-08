1234b08af599d364c9a66bce31c72afcc55b26f4
aLANparty <aLANparty@protonmail.com>
2025-07-07 21:26:15 -0400

    clean up naynar

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { NeynarAuthButton, NeynarContextProvider, Theme } from "@neynar/react";
-import { useState } from "react";
 
 import { PostSettings } from "@/app/components/PostSettings";
 import { BlueskyProvider } from "@/app/services/bluesky/Provider";
@@ -14,51 +13,45 @@ import { TwitterProvider } from "@/app/services/twitter/Provider";
 import { YoutubeProvider } from "@/app/services/youtube/Provider";
 
 export function Post() {
-  // const [user, setUser] = useState<any | null>(null);
-
-  // function signOutLocal() {
-  //   setUser(null);
-  //   localStorage.removeItem("thetoolkit-farcaster-authorization");
-  // }
-
   return (
     <S3Provider>
-      {/* <YoutubeProvider>
+      <YoutubeProvider>
         <InstagramProvider>
           <FacebookProvider>
             <ThreadsProvider>
               <BlueskyProvider>
                 <TiktokProvider>
-                  <TwitterProvider> */}
-      <NeynarContextProvider
-        settings={{
-          clientId: process.env.NEXT_PUBLIC_NEYNAR_CLIENT_ID ?? "",
-          defaultTheme: Theme.Light,
-          eventsCallbacks: {
-            onAuthSuccess: () => {},
-            onSignout: () => {},
-            // onAuthSuccess: ({ user }) => {
-            //   setUser(user);
-            //   localStorage.setItem(
-            //     "thetoolkit-farcaster-authorization",
-            //     JSON.stringify({ user }),
-            //   );
-            // },
-            // onSignout: () => signOutLocal(),
-          },
-        }}
-      >
-        {/* <PostSettings /> */}
+                  <TwitterProvider>
+                    <NeynarContextProvider
+                      settings={{
+                        clientId:
+                          process.env.NEXT_PUBLIC_NEYNAR_CLIENT_ID ?? "",
+                        defaultTheme: Theme.Light,
+                        eventsCallbacks: {
+                          onAuthSuccess: () => {},
+                          onSignout: () => {},
+                          // onAuthSuccess: ({ user }) => {
+                          //   setUser(user);
+                          //   localStorage.setItem(
+                          //     "thetoolkit-farcaster-authorization",
+                          //     JSON.stringify({ user }),
+                          //   );
+                          // },
+                          // onSignout: () => signOutLocal(),
+                        },
+                      }}
+                    >
+                      <PostSettings />
 
-        <NeynarAuthButton label="Sign in with Farcaster" />
-      </NeynarContextProvider>
-      {/* </TwitterProvider>
+                      <NeynarAuthButton label="Sign in with Farcaster" />
+                    </NeynarContextProvider>
+                  </TwitterProvider>
                 </TiktokProvider>
               </BlueskyProvider>
             </ThreadsProvider>
           </FacebookProvider>
         </InstagramProvider>
-      </YoutubeProvider> */}
+      </YoutubeProvider>
     </S3Provider>
   );
 }
