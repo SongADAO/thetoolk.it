9f337945d98fb7dcee2abc325557fdc33232349f
aLANparty <aLANparty@protonmail.com>
2025-07-09 13:35:45 -0400

    clean up hash functions

--- app/lib/hash.ts
+++ app/lib/hash.ts
@@ -3,7 +3,9 @@
  */
 async function sha256Hash(input: string): Promise<string> {
   const data = new TextEncoder().encode(input);
+
   const hash = await globalThis.crypto.subtle.digest("SHA-256", data);
+
   return Array.from(new Uint8Array(hash), (byte) =>
     byte.toString(16).padStart(2, "0"),
   ).join("");
@@ -11,6 +13,7 @@ async function sha256Hash(input: string): Promise<string> {
 
 function djb2Hash(str: string): string {
   let hash = 5381;
+
   for (let i = 0; i < str.length; i++) {
     // eslint-disable-next-line no-bitwise
     hash = (hash << 5) + hash + str.charCodeAt(i);
@@ -20,12 +23,13 @@ function djb2Hash(str: string): string {
   return (hash >>> 0).toString(16);
 }
 
-function objectIdHash(data: object): string {
-  const entries = Object.entries(data).sort(([a], [b]) => a.localeCompare(b));
-
-  const str = entries.map(([key, value]) => `${key}:${String(value)}`).join("");
-
-  return djb2Hash(str);
+function objectIdHash(credentials: object): string {
+  return djb2Hash(
+    Object.entries(credentials)
+      .sort(([a], [b]) => a.localeCompare(b))
+      .map(([key, value]) => `${key}:${String(value)}`)
+      .join(""),
+  );
 }
 
 export { djb2Hash, objectIdHash, sha256Hash };
