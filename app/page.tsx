bf7eba081c709d76667c61331b8ddda299df1d0a
aLANparty <aLANparty@protonmail.com>
2025-07-09 10:33:25 -0400

    storage settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { PostProviders } from "@/app/components/PostProviders";
 import { PostSettings } from "@/app/components/PostSettings";
 import { StorageProviders } from "@/app/components/StorageProviders";
+import { StorageSettings } from "@/app/components/StorageSettings";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <div>
           <header>TheToolk.it</header>
           <main>
+            <StorageSettings />
             <PostSettings />
           </main>
           <footer>TheToolk.it</footer>
