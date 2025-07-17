7c1f63b23543c954c77dc7a360bd36b4c941b309
aLANparty <aLANparty@protonmail.com>
2025-07-17 15:16:09 -0400

    debug mode

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,3 +1,4 @@
+export const DEBUG_MODE = process.env.NEXT_PUBLIC_DEBUG_MODE;
 export const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID;
 export const USE_TESTNET = process.env.NEXT_PUBLIC_USE_TESTNET === "true";
 export const WALLET_CONNECT_PROJECT_ID: string = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ?? "";
