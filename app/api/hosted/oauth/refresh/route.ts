8d93eda85ca4e49242d8d4e8128d5bd007c705df
aLANparty <aLANparty@protonmail.com>
2025-08-06 17:50:07 -0400

    refresh token flow

--- app/api/hosted/oauth/refresh/route.ts
+++ app/api/hosted/oauth/refresh/route.ts
@@ -1,65 +1,30 @@
-import type { SupabaseClient } from "@supabase/supabase-js";
 import { NextRequest } from "next/server";
 
-import { createClient } from "@/lib/supabase/server";
+import { initServerAuth } from "@/lib/supabase/hosted-api";
+import {
+  getServiceAuthorization,
+  updateServiceAuthorization,
+} from "@/lib/supabase/service";
 import { refreshAccessToken } from "@/services/post/facebook/auth";
-
-async function getUser(supabase: SupabaseClient) {
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
-
-  if (error || !user) {
-    throw new Error("Unauthorized");
-  }
-
-  return user;
-}
 
 export async function POST(request: NextRequest) {
   try {
     const { serviceId } = await request.json();
 
-    const supabase = await createClient();
+    const serverAuth = await initServerAuth();
 
-    const user = await getUser(supabase);
-
-    const { data, error } = await supabase
-      .from("services")
-      .select("service_authorization")
-      .eq("user_id", user.id)
-      .eq("service_id", serviceId)
-      .single();
-
-    if (error) {
-      throw new Error("Could not get tokens to refresh from DB");
-    }
-
-    const authorization = data.service_authorization;
-
-    if (!authorization) {
-      throw new Error("Could not get tokens to refresh");
-    }
+    const authorization = await getServiceAuthorization({
+      ...serverAuth,
+      serviceId: "facebook",
+    });
 
     const newAuthorization = await refreshAccessToken(authorization);
 
-    const { error: authorizationError } = await supabase
-      .from("services")
-      .upsert(
-        {
-          service_authorization: newAuthorization,
-          service_id: serviceId,
-          user_id: user.id,
-        },
-        {
-          onConflict: "user_id,service_id",
-        },
-      );
-
-    if (authorizationError) {
-      throw new Error("Could not refresh token");
-    }
+    await updateServiceAuthorization({
+      ...serverAuth,
+      serviceAuthorization: newAuthorization,
+      serviceId,
+    });
 
     return Response.json({ success: true });
   } catch (err: unknown) {
