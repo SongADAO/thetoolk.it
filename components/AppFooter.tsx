695767e37c007016dc0f1a34633e3255f1db4f66
aLANparty <aLANparty@protonmail.com>
2025-10-01 15:56:27 -0400

    update footer license and add github

--- components/AppFooter.tsx
+++ components/AppFooter.tsx
@@ -2,8 +2,28 @@ function AppFooter() {
   return (
     <div className="flex items-center justify-center gap-2 bg-gray-200">
       <div className="flex flex-col items-center justify-center gap-1 p-2 text-center">
-        <div className="flex items-center gap-2">
-          <span>TheToolk.it</span> <span>🅭🅯🄎 GPL 3.0</span>
+        <div className="flex items-center gap-4">
+          <span>TheToolk.it</span>{" "}
+          <span>
+            License:{" "}
+            <a
+              className="text-blue-600 underline hover:text-blue-800"
+              href="https://www.gnu.org/licenses/gpl-3.0.en.html#license-text"
+              target="_blank"
+            >
+              GPLv3
+            </a>
+          </span>
+          <span>
+            Source:{" "}
+            <a
+              className="text-blue-600 underline hover:text-blue-800"
+              href="https://github.com/SongADAO/thetoolk.it"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </span>
         </div>
         <div className="text-xs text-gray-600">
           Created by{" "}
