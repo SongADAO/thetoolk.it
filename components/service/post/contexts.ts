3f3b5af16264a0e2d64f0ed6ef899ba44f50ed00
aLANparty <aLANparty@protonmail.com>
2025-10-01 13:56:52 -0400

    disable tiktok

--- components/service/post/contexts.ts
+++ components/service/post/contexts.ts
@@ -3,7 +3,7 @@ import { FacebookContext } from "@/services/post/facebook/Context";
 import { InstagramContext } from "@/services/post/instagram/Context";
 import { NeynarContext } from "@/services/post/neynar/Context";
 import { ThreadsContext } from "@/services/post/threads/Context";
-import { TiktokContext } from "@/services/post/tiktok/Context";
+// import { TiktokContext } from "@/services/post/tiktok/Context";
 import { TwitterContext } from "@/services/post/twitter/Context";
 import { YoutubeContext } from "@/services/post/youtube/Context";
 
@@ -13,7 +13,7 @@ const POST_CONTEXTS = [
   { context: InstagramContext, id: "InstagramContext" },
   { context: NeynarContext, id: "NeynarContext" },
   { context: ThreadsContext, id: "ThreadsContext" },
-  { context: TiktokContext, id: "TiktokContext" },
+  // { context: TiktokContext, id: "TiktokContext" },
   { context: TwitterContext, id: "TwitterContext" },
   { context: YoutubeContext, id: "YoutubeContext" },
 ];
