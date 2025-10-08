cd45f8673a47d53cffa657df53df27ee4788acb8
aLANparty <aLANparty@protonmail.com>
2025-10-08 17:45:45 -0400

    enable login

--- app/(app)/auth/signup/page.tsx
+++ app/(app)/auth/signup/page.tsx
@@ -1,10 +1,9 @@
-// import SignUpForm from "@/components/Auth/SignUpForm";
+import SignUpForm from "@/components/Auth/SignUpForm";
 
 export default function SignUpPage() {
   return (
     <div className="flex items-center justify-center p-4 md:pt-20">
-      <p>Premium accounts coming soon.</p>
-      {/* <SignUpForm /> */}
+      <SignUpForm />
     </div>
   );
 }
