fc7034b04952d1d1d42cb25638919adc01e34e7d
aLANparty <aLANparty@protonmail.com>
2025-10-29 15:30:33 -0400

    remove tiktok from free

--- components/service/post/PostProgress.tsx
+++ components/service/post/PostProgress.tsx
@@ -3,15 +3,21 @@
 import { POST_CONTEXTS } from "@/components/service/post/contexts";
 import { ServicePostProgressWithContext } from "@/components/service/ServicePostProgressWithContext";
 
-function PostProgress() {
+interface PostProgressProps {
+  mode: "hosted" | "self";
+}
+
+function PostProgress({ mode }: Readonly<PostProgressProps>) {
   return (
     <div className="grid grid-cols-2 gap-2">
-      {POST_CONTEXTS.map((context) => (
-        <ServicePostProgressWithContext
-          context={context.context}
-          key={context.id}
-        />
-      ))}
+      {POST_CONTEXTS.filter((context) => context.modes.includes(mode)).map(
+        (context) => (
+          <ServicePostProgressWithContext
+            context={context.context}
+            key={context.id}
+          />
+        ),
+      )}
     </div>
   );
 }
