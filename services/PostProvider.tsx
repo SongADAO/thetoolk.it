c33196be7bf7bff273bb1d3a7090e2ee502e4d5c
aLANparty <aLANparty@protonmail.com>
2025-11-13 16:43:43 -0500

    remove remotion

--- services/PostProvider.tsx
+++ services/PostProvider.tsx
@@ -11,7 +11,6 @@ import {
 import { sleep } from "@/lib/utils";
 import { convertToHLS, type HLSFiles } from "@/lib/video/hls";
 import { convertVideoMediabunny } from "@/lib/video/mediabunny";
-// import { convertVideoRemotion } from "@/lib/video/remotion";
 import { trimVideo } from "@/lib/video/trim";
 import { downloadFile, getVideoDuration } from "@/lib/video/video";
 import { BlueskyContext } from "@/services/post/bluesky/Context";
