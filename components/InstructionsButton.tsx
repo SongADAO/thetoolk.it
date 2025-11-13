e3ddb3aa72ba30bb79e95efa7a2b0575367c88f1
aLANparty <aLANparty@protonmail.com>
2025-11-13 10:38:36 -0500

    improve button and header layout

---
+++ components/InstructionsButton.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { FaCircleQuestion } from "react-icons/fa6";
+
+function InstructionsButton() {
+  return (
+    <Link
+      className="hidden inline-flex cursor-pointer items-center justify-center gap-2 rounded bg-gray-500 p-3 text-white outline-none hover:bg-gray-800 xl:flex"
+      href="/instructions"
+      target="_blank"
+      title="Instructions"
+    >
+      <FaCircleQuestion className="size-6" />
+    </Link>
+  );
+}
+
+export { InstructionsButton };
