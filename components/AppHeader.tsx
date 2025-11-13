64265ee2080516643a285fbfd262ec76a36c4d35
aLANparty <aLANparty@protonmail.com>
2025-11-13 10:44:24 -0500

    header padding

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -3,7 +3,7 @@ import { AppLogo } from "@/components/AppLogo";
 
 function AppHeader() {
   return (
-    <div className="flex items-center justify-between bg-gray-200 p-2">
+    <div className="flex items-center justify-between bg-gray-200 p-2 lg:px-4">
       <h1>
         <AppLogo />
       </h1>
