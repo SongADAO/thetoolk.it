259958de092aaf339f05b7a8bac6ac42eac59318
aLANparty <aLANparty@protonmail.com>
2025-11-13 11:28:52 -0500

    home

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <div className="mb-10 grid grid-cols-1 items-center justify-center gap-4 p-4 md:grid-cols-[400px_1fr] md:p-4 xl:grid-cols-2">
-        <div className="flex max-w-200 items-center justify-center">
+      <div className="mb-10 grid grid-cols-1 items-center justify-center gap-4 p-4 md:grid-cols-2 md:gap-8 md:p-4">
+        <div className="flex max-w-130 items-center justify-center md:m-auto lg:max-w-160">
           <img
             alt="TheToolk.it Pro Screenshot"
             className="h-auto w-full rounded-lg border-8 border-gray-500 contain-paint"
@@ -15,7 +15,7 @@ export default function Home() {
             TheToolk.it Pro is your one place to post videos to all your
             favorite social media networks at once.
           </p>
-          <h2 className="font-semibold">Supported Networks</h2>
+          <h2 className="mb-2 font-semibold">Supported Networks</h2>
           <div className="grid max-w-100 grid-cols-2 items-start justify-start gap-4 md:flex-row">
             <ul className="list-disc pl-5">
               <li>Bluesky</li>
