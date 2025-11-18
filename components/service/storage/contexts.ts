edec5cb208d1eaa3b489959d5d8b6b52f9af1e87
aLANparty <aLANparty@protonmail.com>
2025-11-18 17:04:45 -0500

    add modes to storage contexts

--- components/service/storage/contexts.ts
+++ components/service/storage/contexts.ts
@@ -4,8 +4,12 @@ import { AmazonS3Context } from "@/services/storage/amazons3/Context";
 import { PinataContext } from "@/services/storage/pinata/Context";
 
 const STORAGE_CONTEXTS = [
-  { context: AmazonS3Context, id: "AmazonS3Context" },
-  { context: PinataContext, id: "PinataContext" },
+  {
+    context: AmazonS3Context,
+    id: "AmazonS3Context",
+    modes: ["hosted", "self"],
+  },
+  { context: PinataContext, id: "PinataContext", modes: ["hosted", "self"] },
 ];
 
 export { STORAGE_CONTEXTS };
