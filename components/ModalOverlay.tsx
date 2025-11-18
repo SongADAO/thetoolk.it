33b9c3c165577519516ed3031f240cc121571d77
aLANparty <aLANparty@protonmail.com>
2025-11-18 17:33:59 -0500

    move components

--- components/ModalOverlay.tsx
+++ components/ModalOverlay.tsx
@@ -1,4 +1,4 @@
-function UpgradeOverlay() {
+function ModalOverlay() {
   return (
     <div className="absolute top-0 right-0 bottom-0 left-0 z-10 h-full w-full bg-[rgba(255,255,255,0.4)] backdrop-blur-[2px]">
       &nbsp;
@@ -6,4 +6,4 @@ function UpgradeOverlay() {
   );
 }
 
-export { UpgradeOverlay };
+export { ModalOverlay };
