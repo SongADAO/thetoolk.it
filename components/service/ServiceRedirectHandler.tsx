4db047f404a45e0da3e3e3ccc948720d0c3bf79c
aLANparty <aLANparty@protonmail.com>
2025-11-18 16:51:10 -0500

    ServiceRedirectHandler

---
+++ components/service/ServiceRedirectHandler.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+
+import { Spinner } from "@/components/Spinner";
+
+interface Props {
+  error: string;
+  handleAuthRedirect: (searchParams: URLSearchParams) => Promise<void>;
+  hasCompletedAuth: boolean;
+  icon: ReactNode;
+  isHandlingAuth: boolean;
+  label: string;
+  loading: boolean;
+}
+
+function ServiceRedirectHandler({
+  error,
+  handleAuthRedirect,
+  hasCompletedAuth,
+  icon,
+  isHandlingAuth,
+  label,
+  loading,
+}: Readonly<Props>) {
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    console.log("Loading", loading);
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    handleAuthRedirect(searchParams);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchParams, loading]);
+
+  if (!isHandlingAuth) {
+    return null;
+  }
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="text-center">
+        <h1 className="mb-8 flex flex-col items-center justify-center gap-2 text-xl">
+          {icon} {label}
+        </h1>
+
+        {hasCompletedAuth ? (
+          <>
+            {error ? (
+              <p className="text-[#f00]">Failed to Authorize</p>
+            ) : (
+              <p className="">Authorization Complete</p>
+            )}
+            <div className="flex items-center justify-center">
+              <button
+                className="mt-4 cursor-pointer rounded bg-gray-500 px-4 py-2 text-white"
+                onClick={() => window.close()}
+                type="button"
+              >
+                Return to TheToolk.it
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="mb-6">Authorizing TheToolk.it</p>
+            <div className="flex items-center justify-center">
+              <Spinner color="black" size="8" />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export { ServiceRedirectHandler };
