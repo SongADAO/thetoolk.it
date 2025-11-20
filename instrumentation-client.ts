fe9aee18827e8604e4c2b6d32c670bc078729a68
aLANparty <aLANparty@protonmail.com>
2025-11-20 15:34:58 -0500

    install sentry
    formatting
    
    format
    
    disable eslint
    
    formatting
    
    disabel tunnelRouting

---
+++ instrumentation-client.ts
@@ -0,0 +1,22 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+// eslint-disable-next-line import/named
+import { captureRouterTransitionStart, init } from "@sentry/nextjs";
+
+init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+});
+
+export const onRouterTransitionStart = captureRouterTransitionStart;
---
+++ instrumentation.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/group-exports, import/dynamic-import-chunkname */
+
+import { captureRequestError } from "@sentry/nextjs";
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./sentry.server.config");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
+  }
+}
+
+export const onRequestError = captureRequestError;
