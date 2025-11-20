7f129c48161684ef2b7effe0056bd9260407c43f
aLANparty <aLANparty@protonmail.com>
2025-11-20 12:31:21 -0500

    remove extra try/catch

--- services/post/neynar/Provider.tsx
+++ services/post/neynar/Provider.tsx
@@ -178,17 +178,12 @@ export function NeynarProvider({ children, mode }: Readonly<Props>) {
       return null;
     }
 
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      triggerLogClientEvent({
-        eventData: {},
-        eventType: "post",
-        serviceId: id,
-      });
-    } catch (err: unknown) {
-      // Allow client logs to fail.
-      console.error("Failed to log client event:", err);
-    }
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    triggerLogClientEvent({
+      eventData: {},
+      eventType: "post",
+      serviceId: id,
+    });
 
     try {
       const accessToken = await getValidAccessToken();
