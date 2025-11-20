5244e030d0b7a1a03e07084ecea773b18761a142
aLANparty <aLANparty@protonmail.com>
2025-11-20 12:04:30 -0500

    logs

--- app/api/hosted/youtube/upload_chunk/route.ts
+++ app/api/hosted/youtube/upload_chunk/route.ts
@@ -1,18 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { logServerPost } from "@/lib/logs";
 import { gateHasActiveSubscription } from "@/lib/subscriptions";
 import { initServerAuth } from "@/lib/supabase/server-auth";
 import { getServiceAuthorizationAndExpiration } from "@/lib/supabase/service";
 import { uploadFileChunk } from "@/services/post/youtube/post";
 
 export async function POST(request: NextRequest) {
+  const serviceId = "youtube";
+
   try {
     const serverAuth = await initServerAuth();
     await gateHasActiveSubscription({ ...serverAuth });
 
     const authorization = await getServiceAuthorizationAndExpiration({
       ...serverAuth,
-      serviceId: "youtube",
+      serviceId,
     });
 
     // Parse FormData instead of JSON
@@ -27,19 +30,33 @@ export async function POST(request: NextRequest) {
     const uploadUrl = String(formData.get("uploadUrl"));
     const uploadedBytes = parseInt(String(formData.get("uploadedBytes")), 10);
 
-    const data = {
+    const response = await uploadFileChunk({
+      accessToken: authorization.authorization.accessToken,
       chunk,
       chunkEnd,
       chunkSize,
       totalBytes,
       uploadUrl,
       uploadedBytes,
-    };
-
-    const response = await uploadFileChunk({
-      ...data,
-      accessToken: authorization.authorization.accessToken,
     });
+
+    if (response.status === 200 || response.status === 201) {
+      const responseData = await response.json();
+
+      await logServerPost({
+        ...serverAuth,
+        postData: {
+          chunkEnd,
+          chunkSize,
+          postId: responseData.id,
+          totalBytes,
+          uploadUrl,
+          uploadedBytes,
+        },
+        serviceId,
+        statusId: 2,
+      });
+    }
 
     return new Response(response.body, {
       status: response.status,
