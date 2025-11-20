739dfd88d1e18c4fc37f44df07b34597e5f3c161
aLANparty <aLANparty@protonmail.com>
2025-11-20 11:40:05 -0500

    log posts

--- app/api/hosted/facebook/videos/route.ts
+++ app/api/hosted/facebook/videos/route.ts
@@ -1,23 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { logServerPost } from "@/lib/logs";
 import { gateHasActiveSubscription } from "@/lib/subscriptions";
 import { initServerAuth } from "@/lib/supabase/server-auth";
 import { getServiceAuthorizationAndExpiration } from "@/lib/supabase/service";
 import { uploadVideo } from "@/services/post/facebook/post";
 
 export async function POST(request: NextRequest) {
+  const serviceId = "facebook";
+
   try {
     const serverAuth = await initServerAuth();
     await gateHasActiveSubscription({ ...serverAuth });
 
     const authorization = await getServiceAuthorizationAndExpiration({
       ...serverAuth,
-      serviceId: "facebook",
+      serviceId,
     });
 
+    const { privacy, text, title, userId, videoUrl } = await request.json();
+
     const postId = await uploadVideo({
-      ...(await request.json()),
       accessToken: authorization.authorization.accessToken,
+      privacy,
+      text,
+      title,
+      userId,
+      videoUrl,
+    });
+
+    await logServerPost({
+      ...serverAuth,
+      postData: {
+        postId,
+        text,
+        title,
+        videoUrl,
+      },
+      serviceId,
+      statusId: 2,
     });
 
     return NextResponse.json({ id: postId });
