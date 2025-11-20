41a4579765c638720e9e85b5be03fd5b0a5ef43f
aLANparty <aLANparty@protonmail.com>
2025-11-20 12:04:30 -0500

    logs

--- app/api/hosted/youtube/videos/route.ts
+++ app/api/hosted/youtube/videos/route.ts
@@ -1,23 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { logServerPost } from "@/lib/logs";
 import { gateHasActiveSubscription } from "@/lib/subscriptions";
 import { initServerAuth } from "@/lib/supabase/server-auth";
 import { getServiceAuthorizationAndExpiration } from "@/lib/supabase/service";
 import { initiateResumableUpload } from "@/services/post/youtube/post";
 
 export async function POST(request: NextRequest) {
+  const serviceId = "youtube";
+
   try {
     const serverAuth = await initServerAuth();
     await gateHasActiveSubscription({ ...serverAuth });
 
     const authorization = await getServiceAuthorizationAndExpiration({
       ...serverAuth,
-      serviceId: "youtube",
+      serviceId,
     });
 
+    const { metadata, videoSize, videoType } = await request.json();
+
     const uploadUrl = await initiateResumableUpload({
-      ...(await request.json()),
       accessToken: authorization.authorization.accessToken,
+      metadata,
+      videoSize,
+      videoType,
+    });
+
+    await logServerPost({
+      ...serverAuth,
+      postData: {
+        metadata,
+        uploadUrl,
+        videoSize,
+        videoType,
+      },
+      serviceId,
+      statusId: 1,
     });
 
     return new Response(null, {
