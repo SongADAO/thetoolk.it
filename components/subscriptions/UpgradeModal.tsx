de31470bb110ce1af67d9218305e1086d2085d87
aLANparty <aLANparty@protonmail.com>
2025-11-24 13:19:27 -0500

    free edition modal

--- components/subscriptions/UpgradeModal.tsx
+++ components/subscriptions/UpgradeModal.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import Link from "next/link";
 import { use } from "react";
 
 import { UserSubscription } from "@/components/account/UserSubscription";
+import { LinkButton } from "@/components/general/LinkButton";
 import { ModalOverlay } from "@/components/general/ModalOverlay";
 import { AuthContext } from "@/contexts/AuthContext";
 
@@ -29,6 +29,7 @@ function UpgradeModal() {
         <ModalOverlay />
         <div className="absolute top-1/4 left-1/2 z-20 w-[90%] max-w-xl -translate-x-1/2 -translate-y-1/2 transform rounded-md border-2 border-gray-500 bg-gray-200 p-6 shadow-lg">
           <div>
+            <h3 className="mb-4 text-center font-bold">TheToolk.it Pro</h3>
             <p className="mb-4 text-center">
               Post videos to all your favorite social networks from one place.
             </p>
@@ -38,30 +39,19 @@ function UpgradeModal() {
               credentials.
             </p>
             <p className="flex flex-col items-center justify-center gap-4 text-center md:flex-row">
-              <Link
-                className="w-full cursor-pointer rounded bg-gray-500 px-4 py-2 text-center text-white hover:bg-gray-800 disabled:opacity-50"
-                href="/auth/signup"
-              >
+              <LinkButton href="/auth/signup" width="full">
                 Create an Account
-              </Link>
-              <Link
-                className="w-full cursor-pointer rounded bg-gray-500 px-4 py-2 text-center text-white hover:bg-gray-800 disabled:opacity-50"
-                href="/auth/signin"
-              >
+              </LinkButton>
+              <LinkButton href="/auth/signin" width="full">
                 Sign In
-              </Link>
+              </LinkButton>
             </p>
           </div>
 
           {/* <div className="mt-10">
             <p className="mb-4 text-center">Not ready to subscribe?</p>
             <p className="text-center">
-              <Link
-                className="cursor-pointer rounded bg-gray-500 px-4 py-2 text-white hover:bg-gray-800 disabled:opacity-50"
-                href="/free"
-              >
-                Try TheToolk.it Free Edition
-              </Link>
+              <LinkButton href="/free">Try TheToolk.it Free Edition</LinkButton>
             </p>
           </div> */}
         </div>
