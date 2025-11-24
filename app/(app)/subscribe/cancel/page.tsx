b42cb31c000169c6a1d62617ccf7392516bd606e
aLANparty <aLANparty@protonmail.com>
2025-11-24 13:16:12 -0500

    link button

--- app/(app)/subscribe/cancel/page.tsx
+++ app/(app)/subscribe/cancel/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import Link from "next/link";
+
+import { LinkButton } from "@/components/general/LinkButton";
 
 export const metadata: Metadata = {
   alternates: {
@@ -21,13 +22,7 @@ export default function SubscribeCancelPage() {
         You cancelled your subscription checkout before completing it. You have
         not been charged.
       </p>
-
-      <Link
-        className="cursor-pointer rounded bg-gray-500 px-4 py-2 text-white hover:bg-gray-800 disabled:opacity-50"
-        href="/pro"
-      >
-        Return to TheToolk.it
-      </Link>
+      <LinkButton href="/pro">Return to TheToolk.it</LinkButton>
     </div>
   );
 }
--- app/(app)/subscribe/success/page.tsx
+++ app/(app)/subscribe/success/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import Link from "next/link";
+
+import { LinkButton } from "@/components/general/LinkButton";
 
 export const metadata: Metadata = {
   alternates: {
@@ -18,12 +19,7 @@ export default function SubscribeSuccessPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 p-4 md:pt-20">
       <p>Thanks for subscribing! Your payment was successful.</p>
-      <Link
-        className="cursor-pointer rounded bg-gray-500 px-4 py-2 text-white hover:bg-gray-800 disabled:opacity-50"
-        href="/pro"
-      >
-        Start Using TheToolk.it Pro
-      </Link>
+      <LinkButton href="/pro">Start Using TheToolk.it Pro</LinkButton>
     </div>
   );
 }
