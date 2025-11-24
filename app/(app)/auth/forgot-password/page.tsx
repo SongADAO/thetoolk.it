b0a593e422c7bd53f4e9693ccc50a3074eecec95
aLANparty <aLANparty@protonmail.com>
2025-11-24 10:57:05 -0500

    rename start-password-reset to forgot password

---
+++ app/(app)/auth/forgot-password/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+import { ForgotPasswordForm } from "@/components/auth/ForgotPasswordForm";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "/auth/forgot-password",
+  },
+  description:
+    "Forgot your password?  Start a password reset by entering your email address.",
+  title: "Forgot Password - TheToolk.it",
+};
+
+export default function ForgotPasswordPage() {
+  return (
+    <div className="flex items-center justify-center p-4 md:pt-20">
+      <div className="w-full">
+        <ForgotPasswordForm />
+      </div>
+    </div>
+  );
+}
