c132ba91d910b977a62677d7a7bbc164c64765a6
aLANparty <aLANparty@protonmail.com>
2025-11-24 13:19:27 -0500

    free edition modal

--- components/poster/FreeBanner.tsx
+++ components/poster/FreeBanner.tsx
@@ -4,12 +4,12 @@ function FreeBanner() {
   return (
     <div className="bg-yellow-100 p-4 text-center">
       <p>
-        You&apos;re using TheToolk.it{" "}
-        <strong className="font-semibold">Free Edition</strong>.{" "}
+        You&apos;re using{" "}
+        <strong className="font-semibold">TheToolk.it Free Edition</strong>.{" "}
         <Link className="underline" href="/pro">
           Switch to TheToolk.it Pro
         </Link>{" "}
-        to unlock Pro features.{" "}
+        to unlock Pro features.
       </p>
     </div>
   );
