8c9b868a3f6cb9e7d43d61808451789237293cb5
aLANparty <aLANparty@protonmail.com>
2025-11-25 18:01:24 -0500

    buttons

--- components/poster/FreeBanner.tsx
+++ components/poster/FreeBanner.tsx
@@ -1,15 +1,14 @@
-import Link from "next/link";
+import { LinkButton } from "@/components/general/LinkButton";
 
 function FreeBanner() {
   return (
-    <div className="bg-yellow-100 p-4 text-center">
+    <div className="space-y-2 bg-yellow-100 p-4 text-center">
       <p>
         You&apos;re using{" "}
-        <strong className="font-semibold">TheToolk.it Free Edition</strong>.{" "}
-        <Link className="underline" href="/pro">
-          Switch to TheToolk.it Pro
-        </Link>{" "}
-        to unlock Pro features.
+        <strong className="font-semibold">TheToolk.it Free Edition</strong>.
+      </p>
+      <p>
+        <LinkButton href="/pro">Switch to TheToolk.it Pro</LinkButton>
       </p>
     </div>
   );
