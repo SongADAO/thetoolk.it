af30bd39d0b5aaf0f23916cbcee611def6f85437
aLANparty <aLANparty@protonmail.com>
2025-11-25 17:35:49 -0500

    poster boxes

--- components/poster/Poster.tsx
+++ components/poster/Poster.tsx
@@ -1,5 +1,9 @@
 import { FaServer, FaUsersGear } from "react-icons/fa6";
 
+import { Box } from "@/components/general/Box";
+import { BoxHeader } from "@/components/general/BoxHeader";
+import { BoxMain } from "@/components/general/BoxMain";
+import { BoxSwitches } from "@/components/general/BoxSwitches";
 import { InstructionsButton } from "@/components/poster/InstructionsButton";
 import { PosterSettingsMenu } from "@/components/poster/PosterSettingsMenu";
 import { PostForm } from "@/components/poster/PostForm";
@@ -20,106 +24,121 @@ function Poster({ mode }: Readonly<PosterProps>) {
   return (
     <article className="grid gap-4 p-2 lg:grid-cols-2 lg:p-4 xl:grid-cols-[1fr_1fr_525px] 2xl:grid-cols-[1fr_1fr_620px]">
       <div>
-        <section className="rounded bg-gray-100 contain-paint">
-          <header className="flex items-center justify-between gap-2 bg-gray-300 p-2 pl-4">
+        <Box>
+          <BoxHeader>
             <h1 className="font-bold">Create Post</h1>
             <div className="flex gap-2 lg:hidden">
               {showInstructions ? <InstructionsButton /> : null}
+
               {showStorageSwitches ? (
                 <PosterSettingsMenu
                   icon={<FaServer className="size-6" />}
                   label="Storage Settings"
                 >
-                  <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
+                  <BoxSwitches>
                     <StorageSwitches mode={mode} />
-                  </div>
+                  </BoxSwitches>
                 </PosterSettingsMenu>
               ) : null}
+
               <PosterSettingsMenu
                 icon={<FaUsersGear className="size-6" />}
                 label="Post Settings"
               >
-                <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
+                <BoxSwitches>
                   <PostSwitches mode={mode} />
-                </div>
+                </BoxSwitches>
               </PosterSettingsMenu>
             </div>
-          </header>
-          <div className="m-4">
+          </BoxHeader>
+          <BoxMain>
             <CreatePostProvider>
               <PostForm />
             </CreatePostProvider>
-          </div>
-        </section>
+          </BoxMain>
+        </Box>
       </div>
 
       <div>
         <div className="flex flex-col gap-4 md:sticky md:top-4">
-          <section className="rounded bg-gray-100 contain-paint">
-            <header className="flex items-center justify-between gap-2 bg-gray-300 p-2 pl-4">
+          <Box>
+            <BoxHeader>
               <h2 className="font-bold">Active Storage Services</h2>
               <div className="hidden gap-2 lg:flex xl:hidden">
                 {showInstructions ? <InstructionsButton /> : null}
+
                 {showStorageSwitches ? (
                   <PosterSettingsMenu
                     icon={<FaServer className="size-6" />}
                     label="Storage Settings"
                   >
-                    <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
+                    <BoxSwitches>
                       <StorageSwitches mode={mode} />
-                    </div>
+                    </BoxSwitches>
                   </PosterSettingsMenu>
                 ) : null}
               </div>
-            </header>
-            <div className="m-4 grid grid-cols-2 gap-2">
-              <StorageProgress mode={mode} />
-            </div>
-          </section>
-          <section className="rounded bg-gray-100 contain-paint">
-            <header className="flex items-center justify-between gap-2 bg-gray-300 p-2 pl-4">
+            </BoxHeader>
+            <BoxMain>
+              <div className="grid grid-cols-2 gap-2">
+                <StorageProgress mode={mode} />
+              </div>
+            </BoxMain>
+          </Box>
+          <Box>
+            <BoxHeader>
               <h2 className="font-bold">Active Posting Services</h2>
               <div className="hidden gap-2 lg:flex xl:hidden">
                 {showInstructions ? <InstructionsButton /> : null}
+
                 <PosterSettingsMenu
                   icon={<FaUsersGear className="size-6" />}
                   label="Post Settings"
                 >
-                  <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
+                  <BoxSwitches>
                     <PostSwitches mode={mode} />
-                  </div>
+                  </BoxSwitches>
                 </PosterSettingsMenu>
               </div>
-            </header>
-            <div className="m-4 grid grid-cols-2 gap-2">
-              <PostProgress mode={mode} />
-            </div>
-          </section>
+            </BoxHeader>
+            <BoxMain>
+              <div className="grid grid-cols-2 gap-2">
+                <PostProgress mode={mode} />
+              </div>
+            </BoxMain>
+          </Box>
         </div>
       </div>
 
       <div className="hidden xl:block">
         <div className="flex flex-col gap-4">
           {showStorageSwitches ? (
-            <section className="rounded bg-gray-100 contain-paint">
-              <header className="flex items-center justify-between gap-2 bg-gray-300 p-2 pl-4">
+            <Box>
+              <BoxHeader>
                 <h2 className="font-bold">Storage Service Settings</h2>
+
                 {showInstructions ? <InstructionsButton /> : null}
-              </header>
-              <div className="m-4 grid grid-cols-1 gap-2 lg:grid-cols-2">
-                <StorageSwitches mode={mode} />
-              </div>
-            </section>
+              </BoxHeader>
+              <BoxMain>
+                <BoxSwitches>
+                  <StorageSwitches mode={mode} />
+                </BoxSwitches>
+              </BoxMain>
+            </Box>
           ) : null}
-          <section className="rounded bg-gray-100 contain-paint">
-            <header className="flex items-center justify-between gap-2 bg-gray-300 p-2 pl-4">
+
+          <Box>
+            <BoxHeader>
               <h2 className="font-bold">Posting Service Settings</h2>
+
               {showInstructions ? <InstructionsButton /> : null}
-            </header>
-            <div className="m-4 grid grid-cols-1 gap-2 lg:grid-cols-2">
-              <PostSwitches mode={mode} />
-            </div>
-          </section>
+            </BoxHeader>
+            <BoxMain>
+              <BoxSwitches>
+                <PostSwitches mode={mode} />
+              </BoxSwitches>
+            </BoxMain>
+          </Box>
         </div>
       </div>
     </article>
