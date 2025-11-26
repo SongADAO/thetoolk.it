6b882c251e9cdca5df5d8e520483e4364185b7a0
aLANparty <aLANparty@protonmail.com>
2025-11-25 19:39:44 -0500

    confirm email

--- app/(app)/auth/confirm-email/page.tsx
+++ app/(app)/auth/confirm-email/page.tsx
@@ -19,15 +19,17 @@ export default function ConfirmEmailPage() {
   return (
     <div className="flex items-center justify-center p-4 md:py-20">
       <div className="w-full">
-        <Suspense
-          fallback={
-            <div className="mx-auto max-w-md space-y-4">
-              <h1 className="text-2xl font-bold">Loading...</h1>
-            </div>
-          }
-        >
-          <ConfirmEmail />
-        </Suspense>
+        <div className="mx-auto max-w-2xl space-y-8">
+          <Suspense
+            fallback={
+              <div className="mx-auto max-w-md space-y-4">
+                <h1 className="text-2xl font-bold">Loading...</h1>
+              </div>
+            }
+          >
+            <ConfirmEmail />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
