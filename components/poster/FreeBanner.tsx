b91d44fba16159b72b877be723cff47c3e8be29e
aLANparty <aLANparty@protonmail.com>
2025-11-25 20:49:02 -0500

    free banner styles

--- components/poster/FreeBanner.tsx
+++ components/poster/FreeBanner.tsx
@@ -2,7 +2,7 @@ import { LinkButton } from "@/components/general/LinkButton";
 
 function FreeBanner() {
   return (
-    <div className="space-y-2 bg-yellow-100 p-4 text-center">
+    <div className="flex flex-col items-center justify-center gap-2 border-b border-gray-400 bg-yellow-100 p-2 text-center md:flex-row md:gap-8">
       <p>
         You&apos;re using{" "}
         <strong className="font-semibold">TheToolk.it Free Edition</strong>.
