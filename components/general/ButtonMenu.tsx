0c6cff99ea8540ab6165e601e7b7b000fd74f6ec
aLANparty <aLANparty@protonmail.com>
2025-12-01 12:15:11 -0500

    button red

--- components/general/ButtonMenu.tsx
+++ components/general/ButtonMenu.tsx
@@ -20,7 +20,7 @@ function ButtonMenu({
 }: Readonly<Props>) {
   return (
     <button
-      className="inline-flex cursor-pointer items-center justify-center gap-3 rounded-xs bg-black p-3 font-semibold text-white no-underline hover:bg-blue-800 disabled:opacity-50 data-[purpose=danger]:bg-red-500 data-[purpose=danger]:hover:bg-red-600 data-[width=full]:w-full"
+      className="inline-flex cursor-pointer items-center justify-center gap-3 rounded-xs bg-black p-3 font-semibold text-white no-underline hover:bg-blue-800 disabled:opacity-50 data-[purpose=danger]:bg-red-700 data-[purpose=danger]:hover:bg-red-600 data-[width=full]:w-full"
       data-purpose={purpose}
       data-width={width}
       disabled={disabled}
