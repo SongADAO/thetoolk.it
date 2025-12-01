6b90e1ec26f5962d99bf6f10f2cec4c1928d456c
aLANparty <aLANparty@protonmail.com>
2025-12-01 15:29:00 -0500

    header button color

--- components/general/LinkButtonMenu.tsx
+++ components/general/LinkButtonMenu.tsx
@@ -20,7 +20,7 @@ function LinkButtonMenu({
 }: Readonly<Props>) {
   return (
     <Link
-      className="inline-flex cursor-pointer items-center justify-center gap-3 rounded-xs border border-gray-400 border-r-black border-b-black bg-white p-3 font-semibold text-black no-underline hover:bg-black hover:text-white disabled:opacity-50 data-[is-active=true]:bg-black data-[is-active=true]:text-white data-[purpose=danger]:bg-red-700 data-[purpose=danger]:hover:bg-red-600 data-[width=full]:w-full"
+      className="inline-flex cursor-pointer items-center justify-center gap-3 rounded-xs border border-gray-400 border-r-black border-b-black bg-white p-3 font-semibold text-black no-underline hover:bg-black hover:text-white disabled:opacity-50 data-[is-active=true]:bg-gray-200 data-[is-active=true]:hover:bg-black data-[purpose=danger]:bg-red-700 data-[purpose=danger]:hover:bg-red-600 data-[width=full]:w-full"
       data-is-active={isActive}
       data-purpose={purpose}
       data-width={width}
